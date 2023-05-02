8507f66abcccc96a9273d36d73aa9d2b167df721
wiktor <wiktor.kostrzewa@interia.pl>
2023-05-02 15:32:46 +0200

    first commit

---
+++ src/components/Node.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { ref } from 'vue'
+const is_open = ref(false)
+const menu_open = ref(0)
+</script>
+
+<template>
+<button class="node" @click="is_open = !is_open">aa</button>
+
+<Teleport to="body">
+  <div v-if="is_open" class="popup-background">
+    <div class="popup">
+      <div class="item-type-container">
+        <button @click="menu_open = 0">Podpora</button>
+        <button @click="menu_open = 1">Siła</button>
+        <button @click="menu_open = 2">Moment</button>
+      </div>
+      <div class="options-container">
+        <form @submit.prevent="send_data___nwm" v-if="menu_open == 0">
+          <div class="support-type-container">
+            <input type="radio" id="Fix" name="support-type" value="Fix">
+            <label for="Fix">Fix</label>
+            <input type="radio" id="roll" name="support-type" value="roll">
+            <label for="roll">roll</label>
+            <input type="radio" id="pin" name="support-type" value="pin">
+            <label for="pin">pin</label>
+          </div>
+          <button type="submit">Accept</button>
+        </form>
+
+        <form @submit.prevent="send_data___nwm" v-if="menu_open == 1">
+          <label>Angle in deg</label>
+          <input type="number">
+          <label>Magnitude</label>
+          <input type="number">
+          <button type="submit">Accept</button>
+        </form>
+
+        <form @submit.prevent="send_data___nwm" v-if="menu_open == 2">
+          <label>Magnitude</label>
+          <input type="number">
+          <button type="submit">Accept</button>
+        </form>
+      </div>
+      <button @click="is_open = !is_open">close</button>
+    </div>
+  </div>
+</Teleport>
+</template>
+
+<style scoped>
+.popup{
+  position: absolute;
+  z-index: 999;
+  top: 20%;
+  left: 15%;
+  border-radius: 10px;
+  background-color: white;
+  padding: 20px;
+  width: 70%;
+}
+
+.node {
+  width: 30px;
+  height: 30px;
+  background-color: red;
+  z-index: 10;
+  text-align: center;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+
+.popup-background{
+  position: fixed;
+  background-color: rgba(0,0,0,0.4);
+  width: 100vw;
+  height: 100vh;
+  z-index: 998;
+  top: 0;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+form *{
+  width: 50%;
+}
+
+form button{
+  margin-top: 10px;
+}
+
+form input {
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+import Node from '@/components/Node.vue'
+
+const nodes = ref([{item: "support", type:"fix"}, {item: "force", angle:90, magn:200}, {item: "support", type:"rail"}]);
+
+function add_node(){
+  if (nodes.value.length < 10){
+    nodes.value.push({item:"empty"});
+  }
+}
+function delete_node(){
+  if (nodes.value.length > 2){
+    nodes.value.pop();
+  }
+}
+</script>
+
+<template>
+  <main>
+    <div class="beam">
+      <Node v-for="node in nodes"/>
+    </div>
+    <div class="node-amount">
+      <button class="add" @click="add_node">Add note</button>
+      <button class="delete" @click="delete_node">delete node</button>
+      <!-- input na numerki ile ich jest i zeby sie dalo zmieniac v bind -->
+    </div>
+  </main>
+</template>
+
+<style>
+.beam {
+  position: relative;
+  border: 1px solid #dfdfdf;
+  border-radius: 10px;
+  height: 5rem;
+  display: flex;
+  justify-content: space-between;
+}
+
+.beam::before {
+  border-top: 5px solid black;
+  content: "";
+  margin: 0 auto;
+  position: absolute;
+  inset: 48% 0 0 0;
+  width: 90%;
+}
+
+</style>
